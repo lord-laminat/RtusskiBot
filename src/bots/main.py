785c667b181eeac52f7e00ff5620e169da50bf03
Konstantin Rasputin <rasputyashka@gmail.com>
2025-04-02 12:58:30 +0300

    syncing from desktop

--- src/bots/main.py
+++ src/bots/main.py
@@ -29,12 +29,14 @@ async def launch():
     tg_posts = asyncio.Queue()
     vk_posts = asyncio.Queue()
 
-    # vkbot = asyncio.create_task(vkontakte_bot.main(vk_config, vk_posts, tg_posts))
+    vkbot = asyncio.create_task(
+        vkontakte_bot.main(vk_config, vk_posts, tg_posts)
+    )
 
     tgbot = asyncio.create_task(
         telegram_bot.main(tg_config, tg_posts, vk_posts, connection_pool)
     )
-    await asyncio.gather(tgbot)  # , tgbot)
+    await asyncio.gather(vkbot, tgbot)  # , tgbot)
 
 
 def main():  # script in pyproject.toml
